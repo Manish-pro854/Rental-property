08e831369d612f6df42be1dab14d32fb4d6a7189
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-05-30 22:33:34 +0530

    complete navbar component with responsive

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import Nav from './component/Nav/Nav'
+import Home from './component/Home/Home'
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
